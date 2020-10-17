3d80ea9256ac4c0a93968b168ac42453b8a96152
Faiz Kidwai <f.y.kidwai@gmail.com>
2020-10-17 20:46:37 +0530

    Added JUnit 5 Tests. Closes #3

---
+++ mssc-beer-service/src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,57 @@
+package guru.springframework.msscbeerservice.web.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+
+    private static final String SLASH = "/";
+    private static final String API_V1_BEER = "/api/v1/beer";
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Test
+    void testGetBeerById() throws Exception {
+        final var id = UUID.randomUUID();
+        mockMvc.perform(get(API_V1_BEER + SLASH + id.toString()).accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    void testCreateNewBeer() throws Exception {
+        final var id = UUID.randomUUID();
+        final var beerDto = BeerDto.builder().id(id).build();
+        final var beerDtoString = mapper.writeValueAsString(beerDto);
+
+        mockMvc.perform(post(API_V1_BEER).contentType(MediaType.APPLICATION_JSON).content(beerDtoString))
+            .andExpect(status().isCreated());
+    }
+
+    @Test
+    void testUpdateBeerById() throws Exception {
+        final var id = UUID.randomUUID();
+        final var beerDto = BeerDto.builder().id(id).build();
+        final var beerDtoString = mapper.writeValueAsString(beerDto);
+
+        mockMvc
+            .perform(put(API_V1_BEER + SLASH + id.toString()).contentType(MediaType.APPLICATION_JSON).content(beerDtoString))
+            .andExpect(status().isNoContent());
+    }
+}
